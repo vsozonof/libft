3500967172cf03f7d0789417c526e5ce37b4eb80
Victor SOZONOFF <vsozonof@e1r12p6.clusters.42paris.fr>
2022-11-19 13:46:14 +0100

    fix calloc and strlcat stp moi du futur

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,15 +6,25 @@
 /*   By: vsozonof <vsozonof@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/19 10:58:04 by vsozonof          #+#    #+#             */
-/*   Updated: 2022/11/19 11:48:41 by vsozonof         ###   ########.fr       */
+/*   Updated: 2022/11/19 13:41:19 by vsozonof         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_is_in_charset(char const *s1, char const *set)
+int	ft_is_in_charset(char c, char const *set)
 {
-	if
+	int	i;
+
+	i = 0;
+	while (set[i])
+	{
+		if (set[i] != c)
+			i++;
+		else
+			return (1);
+	}
+	return (0);
 }
 
 char	*ft_strtrim(char const *s1, char const *set)
